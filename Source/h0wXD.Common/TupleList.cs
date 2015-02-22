27392fa83352ee0a52504c64ecda62a239bb7733
h0wXD <users.noreply@github.com>
2015-02-22 17:08:35 +0100

    Added TupleList & UnitTest

---
+++ Source/h0wXD.Common/TupleList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace h0wXD.Common
+{
+    /// <summary>
+    /// Allows initializing a list of tuples easily using brackets {}
+    /// </summary>
+    /// <typeparam name="T1">Type of item1</typeparam>
+    /// <typeparam name="T2">Type of item2</typeparam>
+    public class TupleList<T1, T2> : List<Tuple<T1, T2>>
+    {
+        public void Add(T1 _item1, T2 _item2)
+        {
+            Add(new Tuple<T1, T2>(_item1, _item2));
+        }
+    }
+    
+    /// <summary>
+    /// Allows initializing a list of tuples easily using brackets {}
+    /// </summary>
+    /// <typeparam name="T1">Type of item1</typeparam>
+    /// <typeparam name="T2">Type of item2</typeparam>
+    /// <typeparam name="T3">Type of item3</typeparam>
+    public class TupleList<T1, T2, T3> : List<Tuple<T1, T2, T3>>
+    {
+        public void Add(T1 _item1, T2 _item2, T3 _item3)
+        {
+            Add(new Tuple<T1, T2, T3>(_item1, _item2, _item3));
+        }
+    }
+}
---
+++ Test/h0wXD.Common.Test/h0wXD.Common.Test/TupleListTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using h0wXD.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace h0wXD.Common.Test
+{
+    [TestClass]
+    public class TupleListTest
+    {
+        [TestMethod]
+        public void Initializer_TwoTypes_AreEqual()
+        {
+            var tupleList = new TupleList<int, int>()
+            {
+                {10, 15},
+                {7, 11},
+            };
+
+            Assert.AreEqual(new Tuple<int, int>(10, 15), tupleList[0]);
+            Assert.AreEqual(new Tuple<int, int>(7, 11), tupleList[1]);
+        }
+
+        [TestMethod]
+        public void Initializer_ThreeTypes_AreEqual()
+        {
+            var tupleList = new TupleList<int, string, int>()
+            {
+                {10, "one", 6},
+                {7, "two", 6},
+            };
+
+            Assert.AreEqual(new Tuple<int, string, int>(10, "one", 6), tupleList[0]);
+            Assert.AreEqual(new Tuple<int, string, int>(7, "two", 6), tupleList[1]);
+        }
+    }
+}
